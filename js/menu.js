38687ec65e951a3b5046a3c29bac21005c4a3326
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2018-12-22 09:42:53 +0200

    theme colors toggle working

--- js/menu.js
+++ js/menu.js
@@ -22,27 +22,41 @@ function menuXX(){
   };
 }
 
-function themeToggle(light,dark){
-  let rootColors = [
-    {tc: light}
-  , 
-    {tb: dark}
-  , 
-    {te: light}
+function themeColors(light, dark) {
+  let rootColors = [{
+      tc: light
+    },
+    {
+      tb: dark
+    },
+    {
+      te: light
+    }
   ];
-  for(index in rootColors){
-    
+  for (index in rootColors) {
+
     let id = Object.getOwnPropertyNames(rootColors[index]);
-    console.log(Object.getOwnPropertyNames(rootColors[index]));
+    //console.log(Object.getOwnPropertyNames(rootColors[index]));
     let value = Object.values(rootColors[index]);
-    console.log(Object.values(rootColors[index]));
+    //console.log(Object.values(rootColors[index]));
     style.setProperty('--' + id, value);
-    console.log(rootColors[index]);
+    //console.log(rootColors[index]);
   }
-  
-  
+}
+
+function themeToggle(id){
+  let themeStateText = document.getElementById(id).innerText
+  //console.log(themeStateText)
+  if(themeStateText == 'Dark-Side'){
+    themeColors('white','black');
+    document.getElementById(id).innerText = 'Light-Side';
+  }else{
+    themeColors('black','white');
+    document.getElementById(id).innerText = 'Dark-Side';
   }
-themeToggle('black','white')
+}
+
+//themeToggle('black','white')
 //themeToggle('white','black')
 
 
