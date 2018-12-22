1071cec0d6ee2a5dbeb56353c7f38fbe71b1deec
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2018-12-22 11:14:29 +0200

    page-header

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,6 @@ let menuOpen = 0;
 let menuClass;
 const style = document.documentElement.style;
 
-
 document.body.addEventListener("click", menuXX);
 
 function menuX(x, st) {
@@ -56,8 +55,33 @@ function themeToggle(id){
   }
 }
 
-//themeToggle('black','white')
-//themeToggle('white','black')
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
 
 
 
