df808ec554ea75e4c8601bfc9a4eb655fa52f5d5
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2018-12-23 23:32:20 +0200

    nodejs for http server

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('D:/Dropbox/Coding/Javascript/shahary.com/shahary.com/indexnew.html')
+});
+
+
+module.exports = router;
