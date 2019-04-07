72033d352e2ae334bf282c161862625d266173f5
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2019-04-07 08:24:28 +0300

    Post form

--- js/menu.js
+++ js/menu.js
@@ -255,3 +255,78 @@ function toggleFullScreen() {
   }
 }
 
+var submitClick = document.getElementById('submit');
+submitClick.onClick = function(){
+sendInfo()
+};
+
+function sendInfo(nameTest, phoneTest, emailTest){
+  
+  if (emailTest != undefined) {
+		let re =
+			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		let res = re.test(String(emailTest.value).toLowerCase());
+		if (!res) {
+			formId.querySelector('.error').innerHTML = 'Sorry but you can\'t use this email, try a real one';
+			test++;
+		}
+	}
+
+	if (nameTest != undefined) {
+		let re =
+			/^[\W \D \S ]{3,100}$/;
+		let res = re.test(String(nameTest.value));
+		if (!res) {
+			formId.querySelector('.error').innerHTML =
+				'The customer name must be at least 3 character long ';
+			test++;
+		}
+	}
+
+	if (phoneTest != undefined) {
+		let re =
+			/^[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{1,6}$/;
+		let res = re.test(String(phoneTest.value));
+		if (!res) {
+			formId.querySelector('.error').innerHTML =
+				'Phone number ex: 000-000-000000). ';
+			test++;
+		}
+	}
+
+
+
+}
+
+
+function postForm(page, formDataNode, id, nextNum) {
+	let i, xhttp, formData;
+
+	formData = new FormData();
+	for (i in formDataNode) {
+		formData.append(formDataNode[i].name, formDataNode[i].value);
+	}
+
+	xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4) {
+			if (this.status == 200) {
+				if (this.response.startsWith('<!DOCTYPE html>')) {
+					document.open();
+					document.write(this.responseText);
+					document.close();
+				} else {
+					contentToView(this.responseText);
+					goToRentTabWithEmail(id, nextNum);
+				}
+			}
+			if (this.status == 404) {
+				contentToView({
+					'title': 'We have a little problem'
+				});
+			}
+		}
+	};
+	xhttp.open('POST', page, true);
+	xhttp.send(formData);
+}
